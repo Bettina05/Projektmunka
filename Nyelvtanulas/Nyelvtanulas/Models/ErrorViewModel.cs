96f1e6c6bf5eb914643ebec1fe5b8f97c1cd3257
GB <ecdl01@vbjnet.hu>
2025-01-23 09:58:03 +0100

    Created mvc

---
+++ Nyelvtanulas/Nyelvtanulas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nyelvtanulas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
