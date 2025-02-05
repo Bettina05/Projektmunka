f6ad9f6d66c5898516804a9db4a3161af5557a36
Bettina Gunics <gunics.bettina.virag@gmail.com>
2025-02-05 20:46:16 +0100

    info and tips are there

---
+++ Nyelvtanulas/Nyelvtanulas/Controllers/InformationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nyelvtanulas.Controllers
+{
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Tips()
+        {
+            return View("~/Views/Information/Tips.cshtml");
+        }
+        public IActionResult Info()
+        {
+            return View("~/Views/Information/Inform.cshtml");
+        }
+
+    }
+}
+
+
